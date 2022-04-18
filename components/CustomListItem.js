6a72b63ebeb27fddc73327f430813170f9c24969
SalmanAhmd <salmanahmed1497@gmail.com>
2022-04-18 18:41:54 +0530

    list chat

---
+++ components/CustomListItem.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Avatar, ListItem } from 'react-native-elements'
+
+const CustomListItem = ({ id, chatName, enterChat }) => {
+  return (
+    <ListItem>
+      <Avatar
+        rounded
+        source={{
+          uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Missing_avatar.svg/1200px-Missing_avatar.svg.png'
+        }}
+      />
+      <ListItem.Content>
+        <ListItem.Title style={styles.title}>
+          Youtube Chat
+        </ListItem.Title>
+        <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
+          Hello
+        </ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
+  )
+}
+
+export default CustomListItem
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: '800'
+  }
+})
